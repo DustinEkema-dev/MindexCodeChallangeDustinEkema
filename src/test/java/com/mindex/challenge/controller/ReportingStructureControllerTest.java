19c0d498027219fcc9453451313472d6ae7c3da7
DustinEkema <dustin.ekema@gmail.com>
2024-07-19 11:00:41 -0400

    remove unneeded port configure

--- src/test/java/com/mindex/challenge/controller/ReportingStructureControllerTest.java
+++ src/test/java/com/mindex/challenge/controller/ReportingStructureControllerTest.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @AutoConfigureMockMvc
 public class ReportingStructureControllerTest {
 
